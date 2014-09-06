e07ebb838b024660f59f6b45352b51b48927c50d
appff <appff@naver.com>
2014-09-06 12:08:57 +0900

    Modify EquvNode

--- soot/src/SignExtractor/EquvNode.java
+++ soot/src/SignExtractor/EquvNode.java
@@ -1,28 +1,23 @@
 package SignExtractor;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class EquvNode {
 	private String Parent;
-	private String Vtype;
-	private List<BFNode> BFlist;
+	private HashMap<String, List<BFNode>> BFlist;
 	
+	public HashMap<String, List<BFNode>> getBFlist() {
+		return BFlist;
+	}
+	public void setBFlist(HashMap<String, List<BFNode>> bFlist) {
+		BFlist = bFlist;
+	}
 	public String getParent() {
 		return Parent;
 	}
 	public void setParent(String parent) {
 		Parent = parent;
 	}
-	public String getVtype() {
-		return Vtype;
-	}
-	public void setVtype(String vtype) {
-		Vtype = vtype;
-	}
-	public List<BFNode> getBFlist() {
-		return BFlist;
-	}
-	public void setBFlist(List<BFNode> bFlist) {
-		BFlist = bFlist;
-	}
+	
 }
